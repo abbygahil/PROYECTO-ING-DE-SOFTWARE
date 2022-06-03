5d5daeadc891971054daceea220cf8bbfa3b0d8e
Depsa Adm <depsa@depsa.gt>
2022-06-02 23:18:33 -0600

    Se agrega el proyecto

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-46EF464\\SQLEXPRESS19;Initial Catalog=FINAL;Integrated Security=True";
+    }
+}
